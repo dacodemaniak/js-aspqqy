861d6066939e00559a04d81bf2d2286e82e12fe2
dacodemaniak <jean-luc.aubert@web-projet.com>
2020-10-05 05:20:37 +0200

    Arrow function implementation

--- index.js
+++ index.js
@@ -15,24 +15,47 @@ function oldWayLog (join) {
   console.log(args.slice(1, args.length).join(join))
 }
 
-class someObject {
+class SomeClass {
   constructor() {
-    this.name = 'Dujardin'
-    this.firstname = 'Jean'
+    this._name = 'Dujardin'
+    this._firstname = 'Jean'
   }
+
+  get name() {
+    return this._name
+  }
+
+  set name(arg) {
+    this.name = arg
+  }
+}
+
+class SomeSpecializedClass extends SomeClass {
+  constructor() {
+    super()
+    this.birthDate = new Date(...['1982','04', '30'])
+  }
+
+  
 }
 
 const callback = () => {
   console.log('Hey, ready now')
   const elements = [...document.querySelectorAll('li')]
   for (const [index, element] of elements.entries()) {
-    console.log('Reading li')
+    console.log(`Reading ${element.innerHTML}`)
   }
 
-  const obj = new someObject()
+  const obj = new SomeClass()
   const {name, firstname } = obj
 
   console.log(`Name : ${name} Firstname : ${firstname}`)
+
+  const specificObj = new SomeSpecializedClass()
+  console.log(`With getter : ${specificObj.name}`)
+
+  const element = elements.find((obj) => obj.innerHTML === 'Item 2')
+  console.log(`${element.innerHTML} is ${elements.indexOf(element) + 1}th item`)
 };
 
 if (
